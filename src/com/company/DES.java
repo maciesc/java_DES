da18b52a07ae04ba67dbb2ba3e469d8370d16e4b
maciesc <32538341+maciesc@users.noreply.github.com>
2018-04-17 17:57:52 +0200

    Debugowanie i zmiany funkcji enkrypcji

--- src/com/company/DES.java
+++ src/com/company/DES.java
@@ -4,58 +4,103 @@ import java.util.Arrays;
 import java.util.BitSet;
 
 public final class DES {
-    private static String message;
-    public DES(){
-        message = "DES Initialization";
-    }
 
+    public static BitSet[] keygenerator(BitSet key){
+        BitSet [] keyTable = new BitSet[16];
+
+        BitSet copyKey = DES.permutatedChoice1(key);
+        //Main.debugXD("56bit",copyKey,7);
+
+        BitSet leftCopyKey = copyKey.get(0,28);
+        //Main.debugXD("C0",leftCopyKey,7);
+        BitSet rigthCopyKey = copyKey.get(28,56);
+        //Main.debugXD("D0",rigthCopyKey,7);
+
+
+
+
+        for(int j = 0; j < 16 ; j++) {
+
+            if (j == 0 || j == 1 || j == 8 || j == 15) {
+                leftCopyKey = DES.leftShift(leftCopyKey);
+                rigthCopyKey = DES.leftShift(rigthCopyKey);
+            } else {
+
+                leftCopyKey = DES.leftShift(leftCopyKey);
+                rigthCopyKey = DES.leftShift(rigthCopyKey);
+                leftCopyKey = DES.leftShift(leftCopyKey);
+                rigthCopyKey = DES.leftShift(rigthCopyKey);
+            }
+
+            //Main.debugXD("C"+(j+1),leftCopyKey,28);
+            //Main.debugXD("D"+(j+1),rigthCopyKey,28);
+            keyTable[j] = append2BitSets(leftCopyKey, rigthCopyKey,28);
+        }
+        return keyTable;
+    }
     public static void encrypt(String fileName,BitSet key){
         byte [] input = FileManager.readBinaryFile(fileName,true); //nadpisywanie inputa -> mniej pamięci
+        BitSet[] keyTable = keygenerator(key);
 
-        BitSet copyKey = (BitSet) key.clone();
         for(int i = 0; i < input.length ; i+=8){
 
-            BitSet temporary = DES.initialPermutation(BitSet.valueOf(Arrays.copyOfRange(input,i,i+8)));
+            byte []blockOf64bits = Arrays.copyOfRange(input,i,i+8);
+
+            BitSet inputBitSet = BitSet.valueOf(blockOf64bits);
+
+            /*BitSet convertedInput = new BitSet(64);
+            for(int k = 0;k <8;k++){
+                int d=k*8;
+                boolean position0 =inputBitSet.get(d);
+                boolean position1 =inputBitSet.get((d+1));
+                boolean position2 =inputBitSet.get(d+2);
+                boolean position3 =inputBitSet.get((d+3));
+                convertedInput.set(d,inputBitSet.get((d)+7));
+                convertedInput.set(d+1,inputBitSet.get((d)+6));
+                convertedInput.set(d+2,inputBitSet.get((d)+5));
+                convertedInput.set(d+3,inputBitSet.get((d)+4));
+                convertedInput.set(d+4,position3);
+                convertedInput.set(d+5,position2);
+                convertedInput.set(d+6,position1);
+                convertedInput.set(d+7,position0);
+            }
+            */
+            BitSet temporary = DES.initialPermutation(inputBitSet);
+            
+
             BitSet left = temporary.get(0,32);
             BitSet right = temporary.get(32,64);
 
-            copyKey = DES.permutatedChoice1(copyKey);
-            BitSet leftCopyKey = copyKey.get(0,28);
-            BitSet rigthCopyKey = copyKey.get(28,56);
+            if(left.size()==0)left = new BitSet(32);
+            if(right.size()==0)right = new BitSet(32);
 
             for(int j = 0; j < 16 ; j++){
 
-                if(j == 0 || j == 1 || j == 8 || j == 15) {
-                    leftCopyKey = DES.leftShift(leftCopyKey);
-                    rigthCopyKey = DES.leftShift(rigthCopyKey);
-                }
-                else{
-
-                    leftCopyKey = DES.leftShift(leftCopyKey);
-                    rigthCopyKey = DES.leftShift(rigthCopyKey);
-                    leftCopyKey = DES.leftShift(leftCopyKey);
-                    rigthCopyKey = DES.leftShift(rigthCopyKey);
-                }
-
-                BitSet outputKey = append2BitSets(leftCopyKey, rigthCopyKey,28);
-
+                BitSet outputKey = keyTable[j];
                 outputKey = permutatedChoice2(outputKey);
 
                 BitSet expandedRight = expansionPermutation(right);
+
                 expandedRight.xor(outputKey);
 
 
                 BitSet right32b = decreaseKeyTo32b(expandedRight);
+
                 right32b = permutate32bitKey(right32b);
+
+
                 left.xor(right32b);
 
                 BitSet pom = left;
                 left = right;
                 right = pom;
             }
-            //zmiana
+
+
             BitSet almostFinished = append2BitSets(right,left,32);
+
             almostFinished = reversedInitialPermutation(almostFinished);
+
 
             byte[] array = almostFinished.toByteArray();
 
@@ -66,39 +111,22 @@ public final class DES {
                    input[i+k] = 0;
                 }
         }
-        FileManager.writeBinaryFile("C:\\Users\\Maciej\\Desktop\\DESoutput.bin",input,input.length);
+        FileManager.writeBinaryFile("C:\\Users\\Maciej\\Desktop\\encrypted.bin",input,input.length);
     }
 
     public static void decrypt(String fileName,BitSet key){
         byte [] input = FileManager.readBinaryFile(fileName,false); //nadpisywanie inputa -> mniej pamięci
-        BitSet copyKey = (BitSet) key.clone();
+        //BitSet copyKey = (BitSet) key.clone();
+        BitSet[] keyTable = keygenerator(key);
         for(int i = 0; i < input.length ; i+=8){
 
             BitSet temporary = DES.initialPermutation(BitSet.valueOf(Arrays.copyOfRange(input,i,i+8)));
             BitSet left = temporary.get(0,32);
             BitSet right = temporary.get(32,64);
 
-            copyKey = DES.permutatedChoice1(copyKey);
-            BitSet leftCopyKey = copyKey.get(0,28);
-            BitSet rigthCopyKey = copyKey.get(28,56);
-            //leftCopyKey = leftShift32X(leftCopyKey);
-            //rigthCopyKey = leftShift32X(rigthCopyKey);
+            for(int j = 0; j < 16 ; j++){
 
-            for(int j = 15; j >= 0 ; j--){
-
-                if(j == 0 || j == 1 || j == 8 || j == 15) {
-                    leftCopyKey = DES.rightShift(leftCopyKey);
-                    rigthCopyKey = DES.rightShift(rigthCopyKey);
-                }
-                else{
-
-                    leftCopyKey = DES.rightShift(leftCopyKey);
-                    rigthCopyKey = DES.rightShift(rigthCopyKey);
-                    leftCopyKey = DES.rightShift(leftCopyKey);
-                    rigthCopyKey = DES.rightShift(rigthCopyKey);
-                }
-
-                BitSet outputKey = append2BitSets(leftCopyKey, rigthCopyKey,28);
+                BitSet outputKey = keyTable[15-j];
                 outputKey = permutatedChoice2(outputKey);
 
                 BitSet expandedRight = expansionPermutation(right);
@@ -108,10 +136,11 @@ public final class DES {
                 BitSet right32b = decreaseKeyTo32b(expandedRight);
                 right32b = permutate32bitKey(right32b);
                 left.xor(right32b);
-
                 BitSet pom = right;
+
                 right = left;
                 left = pom;
+
             }
             BitSet almostFinished = append2BitSets(right,left,32);
             almostFinished = reversedInitialPermutation(almostFinished);
@@ -125,7 +154,7 @@ public final class DES {
                 input[i+k] = 0;
             }
         }
-        int index = 0;
+        int index = input.length;
         for(int i = input.length-1; i >=0; i--){
             if(input[i]==1)
             {
@@ -133,7 +162,7 @@ public final class DES {
                 break;
             }
         }
-        FileManager.writeBinaryFile("C:\\Users\\Maciej\\Desktop\\DESdecryptOutput.bin",input,input.length);
+        FileManager.writeBinaryFile("C:\\Users\\Maciej\\Desktop\\decrypted.bin",input,index);
     }
 
     private static BitSet append2BitSets(BitSet firstHalf, BitSet secondHalf,int numberOfBits) {
@@ -148,22 +177,12 @@ public final class DES {
     //2
     public static BitSet initialPermutation(BitSet tableOf64bits){
 
-        BitSet IP_array = new BitSet(64);
-        int index = 0;
-        //Dla parzystych
-        for(int row = 57; row <= 63; row+=2)
-            for (int column = row; column > 0; column -= 8)
-                IP_array.set(index++, tableOf64bits.get(column));
+        BitSet result = new BitSet(64);
 
+        for (int i = 0 ; i < 64 ; i++)
+            result.set(i, tableOf64bits.get(Tables.IP[i]-1));
 
-        //Dla nieparzystych
-        for(int row = 56; row <= 62; row+=2)
-            for(int column = row; column > 0; column-=8)
-                IP_array.set(index++,tableOf64bits.get(column));
-
-
-
-        return IP_array;
+        return result;
     }
 
     //4
@@ -208,20 +227,6 @@ public final class DES {
         return LF_array;
     }
 
-    public static BitSet rightShift(BitSet tableOf28bits){
-        BitSet R_array = new BitSet(28);
-        for(int i = 1; i < 28; i++)
-            R_array.set(i,tableOf28bits.get(i-1));
-        R_array.set(0,tableOf28bits.get(27));
-        return R_array;
-    }
-
-    public static BitSet leftShift32X(BitSet tableOf28bits){
-        BitSet LF_array = tableOf28bits;
-        for(int i = 0; i < 4; i++)
-        LF_array= leftShift(LF_array);
-        return LF_array;
-    }
     //7
     public static BitSet permutatedChoice2(BitSet tableOf56bits){
         BitSet PC2_array = new BitSet(48);
@@ -255,15 +260,6 @@ public final class DES {
         return EP_array;
     }
 
-    //9
-    public static BitSet xor48bKeys(BitSet R, BitSet K)
-    {
-        BitSet key = new BitSet(48);
-        for (int i = 0 ; i < 48 ; i++)
-            key.set(i, R.get(i) ^ K.get(i));
-        return key;
-    }
-
     //10, 11, 12, 13
     public static BitSet decreaseKeyTo32b(BitSet key48b)
     {
@@ -284,6 +280,7 @@ public final class DES {
             }
 
             row = (key48b.get(index+1) ? 1000 : 0) + (key48b.get(index+2) ? 100 : 0) + (key48b.get(index+3) ? 10 : 0) + (key48b.get(index+4) ? 1 : 0);
+
             switch (row){
                 case 0: row = 0; break;
                 case 1111: row = 15; break;
@@ -305,12 +302,18 @@ public final class DES {
             }
             int[][] localS = Tables.getS();
             String resultBinStr = Integer.toBinaryString(localS[i][row  + column * 16]);
+
+
             for (int j = 0 ; j < resultBinStr.length() ; j++)
             {
-                //TU to moze nie dzialac
-                if (resultBinStr.codePointAt(j) == '1')
-                    resultBitSet.set(i*4+j + 4-resultBinStr.length());
+                //Napewno dziala testowalem
+
+                if (resultBinStr.codePointAt(j) == '1') {
+                    resultBitSet.set(i * 4 + j + 4 - resultBinStr.length());
+                }
+
             }
+
         }
 
         return resultBitSet;
@@ -327,55 +330,15 @@ public final class DES {
         return result;
     }
 
-    //15
-    public static BitSet xorLR(BitSet R, BitSet L)
-    {
-        BitSet Rplus1 = new BitSet(32);
-        for (int i = 0 ; i < 32 ; i++)
-            Rplus1.set(i, R.get(i) ^ L.get(i));
-        return Rplus1;
-    }
-
     //18
     public static BitSet reversedInitialPermutation(BitSet tableOf64bits)
     {
         BitSet result = new BitSet(64);
 
         for (int i = 0 ; i < 64 ; i++)
-            result.set(i, tableOf64bits.get(Tables.REVERSED_IP[i]));
+            result.set(i, tableOf64bits.get(Tables.REVERSED_IP[i]-1));
 
         return result;
     }
 
-    //konwerter longow na bitsety
-    public static BitSet convert(long value) {
-        BitSet bits = new BitSet();
-        int index = 0;
-        while (value != 0L) {
-            if (value % 2L != 0) {
-                bits.set(index);
-            }
-            ++index;
-            value = value >>> 1;
-        }
-        return bits;
-    }
-
-    // nie działa
-    public static BitSet paddingAdding(BitSet data){
-        int bitSetModulo = data.size() % 64;
-
-        data.set(data.size(),true);
-        data.set(data.size(),data.size()+(63-bitSetModulo),false);
-        return data;
-    }
-
-    //konwerter bitsetow na longi
-    public static long convert(BitSet bits) {
-        long value = 0L;
-        for (int i = 0; i < bits.length(); ++i) {
-            value += bits.get(i) ? (1L << i) : 0L;
-        }
-        return value;
-    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,69 @@ public class Main {
         BitSet xd = new BitSet(64);
         byte[] xdb = {43,54,86,-10,-64,100,1,43};
         xd = BitSet.valueOf(xdb);
+        BitSet key = new BitSet(64);
 
-        DES.encrypt("C:\\Users\\Maciej\\Desktop\\BSK\\3 i 4\\JD.bin",xd);
-        DES.decrypt("C:\\Users\\Maciej\\Desktop\\DESoutput.bin",xd);
+        key.set(3);
+        key.set(6);
+        key.set(7);
 
+        key.set(10);
+        key.set(11);
+        key.set(13);
+
+        key.set(17);
+        key.set(19);
+        key.set(21);
+        key.set(22);
+        key.set(23);
+
+        key.set(25);
+        key.set(26);
+        key.set(27);
+        key.set(28);
+        key.set(31);
+
+        key.set(32);
+        key.set(35);
+        key.set(36);
+        key.set(38);
+        key.set(39);
+        key.set(40);
+        key.set(42);
+        key.set(43);
+        key.set(44);
+        key.set(45);
+        key.set(48);
+        key.set(49);
+        key.set(51);
+        key.set(52);
+        key.set(53);
+        key.set(54);
+        key.set(55);
+        key.set(56);
+        key.set(57);
+        key.set(58);
+        key.set(59);
+        key.set(63);
+
+
+        //byte[] message = {1,23,45,67,89,0xA,0xB,0xC,0xD,0xE,0xF};
+        //FileManager.writeBinaryFile("C:\\Users\\Maciej\\Desktop\\dataTotest.bin",message,message.length);
+
+
+        DES.encrypt("C:\\Users\\Maciej\\Desktop\\BSK\\3 i 4\\JD.bin",key);
+        DES.decrypt("C:\\Users\\Maciej\\Desktop\\encrypted.bin",key);
+
+    }
+    public static void debugXD(String message,BitSet key,int modulo){
+        System.out.println(message);
+        for(int i = 0;i<key.size();i++){
+            System.out.print(key.get(i)?1:0);
+            if (i % modulo == modulo-1) {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.println();
     }
 }
